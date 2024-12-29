0865135d6c00c85a2094361d326e8a5c30cd10c2
sumitshinde2006 <sumit.shinde_24uds@sanjivani.edu.in>
2024-12-30 05:07:05 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,13 @@
 public class Hello{
 	public static void main(String args[])
 	{
-		System.out.println("Hello Guyz!");
-		System.out.println("aai ghala");
+		System.out.println("nano HelloWorld.java");
+		System.out.println("git init");
+		System.out.println("git add HelloWorld.java");
+		System.out.println("git commit -m "Initial commit"");
+		System.out.println("git remote add origin https://github.com/your-username/HelloWorldApp.git");
+		System.out.println("git branch -M main");
+		System.out.println("git push -u origin main");
 
 	}
 }
